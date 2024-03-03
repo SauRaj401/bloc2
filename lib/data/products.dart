b6ed34880d7884f3cdd63e909c263dc880a3ab17
Saurav Sunar <sunarsaurav01@gmail.com>
2024-03-03 17:14:04 +0545

    bloc2 project

---
+++ lib/data/products.dart
@@ -0,0 +1,54 @@
+class Products {
+  static List<Map<String, dynamic>> groceryProducts = [
+    {
+      'id': 1,
+      'name': 'Smartphone',
+      'description': 'A powerful smartphone with advanced features.',
+      'price': 599.99,
+      'imageURL':
+          'https://www.trustedreviews.com/wp-content/uploads/sites/54/2023/11/Best-smartphone-3-920x518.jpg',
+    },
+    {
+      'id': 2,
+      'name': 'Laptop',
+      'description': 'High-performance laptop for work and entertainment.',
+      'price': 1299.99,
+      'imageURL':
+          'https://cdn.thewirecutter.com/wp-content/media/2023/06/bestlaptops-2048px-9765.jpg?auto=webp&quality=75&width=1024',
+    },
+    {
+      'id': 3,
+      'name': 'Headphones',
+      'description': 'Wireless headphones with noise-cancelling technology.',
+      'price': 199.99,
+      'imageURL':
+          'https://audioshopnepal.com/wp-content/uploads/2023/05/2305231434590-1.jpg',
+    },
+    {
+      'id': 4,
+      'name': 'Television',
+      'description': 'Ultra HD Smart TV with built-in streaming services.',
+      'price': 899.99,
+      'imageURL':
+          'https://m.media-amazon.com/images/I/81dB1RUVvdL._AC_SL1500_.jpg',
+    },
+    {
+      'id': 5,
+      'name': 'Coffee Maker',
+      'description': 'Programmable coffee maker with auto-brew feature.',
+      'price': 49.99,
+      'imageURL':
+          'https://somethingsbrewing.in/cdn/shop/files/6_82f413ee-0a10-4f28-941f-fb55450c3a13.jpg?v=1688453075&width=1445',
+    },
+    {
+      'id': 6,
+      'name': 'Fitness Tracker',
+      'description': 'Track your activity, heart rate, and sleep patterns.',
+      'price': 79.99,
+      'imageURL':
+          'https://www.popsci.com/uploads/2023/09/22/best-cheap-fitness-trackers.jpg',
+    }
+
+    // Add more products as needed
+  ];
+}
